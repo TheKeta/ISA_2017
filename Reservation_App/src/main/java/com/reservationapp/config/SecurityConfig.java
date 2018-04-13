4b936448f6144ab0c1d2b03c57cdd3cf356c9e69
Stefan Radanovic <radanovicstefan@gmail.com>
2018-04-13 18:50:51 +0200

    a

--- Reservation_App/src/main/java/com/reservationapp/config/SecurityConfig.java
+++ Reservation_App/src/main/java/com/reservationapp/config/SecurityConfig.java
@@ -1,76 +1,59 @@
 package com.reservationapp.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.autoconfigure.security.SecurityProperties;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
-//ovde
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@Order(SecurityProperties.BASIC_AUTH_ORDER)
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	@Autowired
-    private UserDetailsService userDetailsService;
- 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/", "/public/**").permitAll()
-//                .antMatchers("/users/**").hasAuthority("ADMIN")
-//                .anyRequest().fullyAuthenticated()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                .failureUrl("/login")
-//                .usernameParameter("email")
-//                .permitAll()
-//                .and()
-//                .logout()
-//                .logoutUrl("/logout")
-//                .deleteCookies("remember-me")
-//                .logoutSuccessUrl("/")
-//                .permitAll()
-//                .and()
-//                .rememberMe()
-//                .and()
-//                .sessionManagement()
-//                .maximumSessions(1);
-
-    	http.authorizeRequests()
-        .antMatchers("/Create.html").hasAuthority("ADMIN")
-        .anyRequest().permitAll()
-        .and()
-        .formLogin()
-        .loginPage("/Login.html")
-        .failureUrl("/Login.html")
-        .usernameParameter("email")
-        .permitAll()
-        .and()
-        .logout()
-        .logoutUrl("/logout")
-        .deleteCookies("remember-me")
-        .logoutSuccessUrl("/Home.html")
-        .permitAll()
-        .and()
-        .rememberMe()
-        .and()
-        .sessionManagement()
-        .maximumSessions(1);
-    }
-
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(new BCryptPasswordEncoder());
-    }
+public class SecurityConfig{
+	
 }
+//
+////ovde
+//@Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@Order(SecurityProperties.BASIC_AUTH_ORDER)
+//public class SecurityConfig extends WebSecurityConfigurerAdapter{
+//	@Autowired
+//    private UserDetailsService userDetailsService;
+// 
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+////        http.authorizeRequests()
+////                .antMatchers("/", "/public/**").permitAll()
+////                .antMatchers("/users/**").hasAuthority("ADMIN")
+////                .anyRequest().fullyAuthenticated()
+////                .and()
+////                .formLogin()
+////                .loginPage("/login")
+////                .failureUrl("/login")
+////                .usernameParameter("email")
+////                .permitAll()
+////                .and()
+////                .logout()
+////                .logoutUrl("/logout")
+////                .deleteCookies("remember-me")
+////                .logoutSuccessUrl("/")
+////                .permitAll()
+////                .and()
+////                .rememberMe()
+////                .and()
+////                .sessionManagement()
+////                .maximumSessions(1);
+//
+//    	http.authorizeRequests()
+//    	.antMatchers("/", "/**", "/h2-console/**").permitAll()
+//        .anyRequest().permitAll();
+//    }
+//
+//
+//}
--- Reservation_App/src/main/java/com/reservationapp/model/CurrentUser.java
+++ Reservation_App/src/main/java/com/reservationapp/model/CurrentUser.java
@@ -1,30 +1,34 @@
 package com.reservationapp.model;
 
-import org.springframework.security.core.authority.AuthorityUtils;
+//import org.springframework.security.core.authority.AuthorityUtils;
 
-public class CurrentUser extends org.springframework.security.core.userdetails.User{
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -1134350725709383142L;
-
-	private User user;
+public class CurrentUser{
 	
-	public CurrentUser(User user){
-		super(user.getEmail(), user.getPassword(), AuthorityUtils.createAuthorityList(user.getUserType().getName()));
-		this.user = user;
-	}
-
-	public User getUser() {
-		return user;
-	}
-	
-	public Long getId() {
-        return user.getId();
-    }
-	
-	UserType getUserType(){
-		return user.getUserType();
-	}
 }
+//public class CurrentUser extends org.springframework.security.core.userdetails.User{
+//
+//	/**
+//	 * 
+//	 */
+//	private static final long serialVersionUID = -1134350725709383142L;
+//
+//	private User user;
+//	
+//	public CurrentUser(User user){
+//		super(user.getEmail(), user.getPassword(), AuthorityUtils.createAuthorityList(user.getUserType().getName()));
+//		this.user = user;
+//	}
+//
+//	public User getUser() {
+//		return user;
+//	}
+//	
+//	public Long getId() {
+//        return user.getId();
+//    }
+//	
+//	UserType getUserType(){
+//		return user.getUserType();
+//	}
+//}
