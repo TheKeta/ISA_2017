d4a4d48a8e73c59da39c5cbfd20081699362cd3e
Stefan Radanovic <radanovicstefan@gmail.com>
2018-04-17 23:07:25 +0200

    registration and login

--- Reservation_App/src/main/java/com/reservationapp/config/SecurityConfig.java
+++ Reservation_App/src/main/java/com/reservationapp/config/SecurityConfig.java
@@ -1,59 +1,94 @@
 package com.reservationapp.config;
 
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.autoconfigure.security.SecurityProperties;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.annotation.Order;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
-public class SecurityConfig{
+//ovde
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	@Autowired
+    private UserDetailsService userDetailsService;
+ 
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth)
+			throws Exception {
+		auth
+			.userDetailsService(userDetailsService)
+			.passwordEncoder(bCryptPasswordEncoder);
+	}
+	
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/", "/public/**").permitAll()
+//                .antMatchers("/users/**").hasAuthority("ADMIN")
+//                .anyRequest().fullyAuthenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .failureUrl("/login")
+//                .usernameParameter("email")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .logoutUrl("/logout")
+//                .deleteCookies("remember-me")
+//                .logoutSuccessUrl("/")
+//                .permitAll()
+//                .and()
+//                .rememberMe()
+//                .and()
+//                .sessionManagement()
+//                .maximumSessions(1);
+    	
+    	http.
+		authorizeRequests()
+			.antMatchers("/").permitAll()
+			.antMatchers("/h2-console", "/h2-console/**").permitAll()
+			.antMatchers("/login").permitAll()
+			.antMatchers("/registration").permitAll()
+			.antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
+			.authenticated().and().csrf().disable().formLogin()
+			.loginPage("/login").failureUrl("/login?error=true")
+			.defaultSuccessUrl("/admin/home")
+			.usernameParameter("email")
+			.passwordParameter("password")
+			.and().logout()
+			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+			.logoutSuccessUrl("/").and().exceptionHandling()
+			.accessDeniedPage("/access-denied");
+
+    	http.headers().frameOptions().disable();
+    }
+    
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	    web
+	       .ignoring()
+	       .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		return bCryptPasswordEncoder;
+	}
+
 }
-//
-////ovde
-//@Configuration
-//@EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
-//@Order(SecurityProperties.BASIC_AUTH_ORDER)
-//public class SecurityConfig extends WebSecurityConfigurerAdapter{
-//	@Autowired
-//    private UserDetailsService userDetailsService;
-// 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-////        http.authorizeRequests()
-////                .antMatchers("/", "/public/**").permitAll()
-////                .antMatchers("/users/**").hasAuthority("ADMIN")
-////                .anyRequest().fullyAuthenticated()
-////                .and()
-////                .formLogin()
-////                .loginPage("/login")
-////                .failureUrl("/login")
-////                .usernameParameter("email")
-////                .permitAll()
-////                .and()
-////                .logout()
-////                .logoutUrl("/logout")
-////                .deleteCookies("remember-me")
-////                .logoutSuccessUrl("/")
-////                .permitAll()
-////                .and()
-////                .rememberMe()
-////                .and()
-////                .sessionManagement()
-////                .maximumSessions(1);
-//
-//    	http.authorizeRequests()
-//    	.antMatchers("/", "/**", "/h2-console/**").permitAll()
-//        .anyRequest().permitAll();
-//    }
-//
-//
-//}
--- Reservation_App/src/main/java/com/reservationapp/model/User.java
+++ Reservation_App/src/main/java/com/reservationapp/model/User.java
@@ -9,6 +9,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.data.annotation.Transient;
+
+@SuppressWarnings("deprecation")
 @Entity
 public class User implements Serializable{
 
@@ -19,18 +25,36 @@ public class User implements Serializable{
 	private Long id;
 	
 	@Column(nullable = false)
+	@Email(message = "*Please provide a valid Email")
+	@NotEmpty(message = "*Please provide an email")
 	private String email;
 	
 	@Column(nullable = false)
 	private String username;
 	
 	@Column(nullable = false)
+	private boolean active;
+	
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	@Column(nullable = false)
+	@Length(min = 5, message = "*Your password must have at least 5 characters")
+	@NotEmpty(message = "*Please provide your password")
+	@Transient
 	private String password;
 	
 	@Column(nullable = false)
+	@NotEmpty(message = "*Please provide your first name")
 	private String firstName;
 	
 	@Column(nullable = false)
+	@NotEmpty(message = "*Please provide your last name")
 	private String lastName;
 	
 	@ManyToOne(optional = true)
