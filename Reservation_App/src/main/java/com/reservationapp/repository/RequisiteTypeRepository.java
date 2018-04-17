3a2ae5eb70a1025950f0113fd5c476b0cd273fb3
TheKeta <TheKeta@DESKTOP-IL15GLK>
2018-04-18 01:05:42 +0200

    Requsite Type removed

--- Reservation_App/src/main/java/com/reservationapp/repository/RequisiteTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.reservationapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.reservationapp.model.RequisiteType;
-
-public interface RequisiteTypeRepository extends JpaRepository<RequisiteType, Long>{
-
-}
