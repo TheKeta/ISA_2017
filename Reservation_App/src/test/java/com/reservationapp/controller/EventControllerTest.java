dd72d3716a3afa6ffced8ed9c0d299835f7ea5fe
Dejan Besic <dejanbesic@yahoo.com>
2018-04-22 04:13:37 +0200

    Tests

--- Reservation_App/src/test/java/com/reservationapp/controller/EventControllerTest.java
+++ Reservation_App/src/test/java/com/reservationapp/controller/EventControllerTest.java
@@ -1,20 +1,26 @@
 package com.reservationapp.controller;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.nio.charset.Charset;
 import java.util.Date;
 
+import javax.annotation.PostConstruct;
+
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.reservationapp.model.Event;
 import com.reservationapp.model.Hall;
@@ -22,10 +28,7 @@ import com.reservationapp.model.Show;
 
 import main.TestUtil;
 import test.constants.EventConstants;
-import test.constants.RequisiteConstants;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class EventControllerTest {
 
 	private static final String URL_PREFIX = "/event";
@@ -35,11 +38,18 @@ public class EventControllerTest {
 			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 
 
-	
 	private MockMvc mockMvc;
 	
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@PostConstruct
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
 	
-	void testAddEvent() throws Exception {
+	@Test
+	public void testAddEvent() throws Exception {
 		Event event = new Event();
 		event.setEventDate(new Date());
 		event.setHall(new Hall());
@@ -54,9 +64,64 @@ public class EventControllerTest {
 	@Test
 	@Transactional
 	@Rollback(true)
-	void testDelete() throws Exception {
+	public void testDelete() throws Exception {
 		this.mockMvc.perform(delete(URL_PREFIX + "/" + EventConstants.DB_ID)).andExpect(status().isOk());
+	}
 
+	@Test
+	public void testGetEvents() throws Exception {
+		this.mockMvc.perform(get(URL_PREFIX + "/events/" + EventConstants.DB_ID)).andExpect(status().isOk())
+				.andExpect(content().contentType(contentType)).andExpect(jsonPath("$", hasSize(2)))
+				.andExpect(jsonPath("$.[*].id").value(EventConstants.DB_ID))
+				.andExpect(jsonPath("$.[*].hall").value(EventConstants.DB_HALL))
+				.andExpect(jsonPath("$.[*].show").value(EventConstants.DB_SHOW))
+				.andExpect(jsonPath("$.[*].eventDate").value(EventConstants.DB_EVENTDATE))
+				.andExpect(jsonPath("$.[*].price").value(EventConstants.DB_PRICE));
+	}
+
+	@Test
+	public void testGetEventSeats() throws Exception {
+		this.mockMvc.perform(get(URL_PREFIX + "/getEventSeats/" + EventConstants.DB_ID)).andExpect(status().isOk())
+			.andExpect(content().contentType(contentType)).andExpect(jsonPath("$", hasSize(3)))
+			.andExpect(jsonPath("$.[*].event").value(EventConstants.DB_EVENT))
+			.andExpect(jsonPath("$.[*].seats").value(EventConstants.DB_SEATS))
+			.andExpect(jsonPath("$.[*].reservations").value(EventConstants.DB_RESERVATIONS));
+	}
+
+	@Test
+	public void testGetEventShows() throws Exception {
+		this.mockMvc.perform(get(URL_PREFIX + "/" + EventConstants.DB_ID)).andExpect(status().isOk())
+		.andExpect(content().contentType(contentType)).andExpect(jsonPath("$", hasSize(3)))
+		.andExpect(jsonPath("$.[*].event").value(EventConstants.DB_EVENT))
+		.andExpect(jsonPath("$.[*].shows").value(EventConstants.DB_SHOWS));
+	}
+
+	@Test
+	public void testGetEventShowsHalls() throws Exception {
+		this.mockMvc.perform(get(URL_PREFIX + "/eventShowsHalls/" + EventConstants.DB_ID)).andExpect(status().isOk())
+			.andExpect(content().contentType(contentType)).andExpect(jsonPath("$", hasSize(3)))
+			.andExpect(jsonPath("$.[*].event").value(EventConstants.DB_EVENT))
+			.andExpect(jsonPath("$.[*].shows").value(EventConstants.DB_SHOWS))
+			.andExpect(jsonPath("$.[*].halls").value(EventConstants.DB_HALLS));
+		;
+	}
+
+	@Test
+	public void testGetEvent() throws Exception {
+		this.mockMvc.perform(get(URL_PREFIX + "/getEvent/" + EventConstants.DB_ID)).andExpect(status().isOk())
+		.andExpect(content().contentType(contentType))
+		.andExpect(jsonPath("$.[*].id").value(EventConstants.DB_ID))
+		.andExpect(jsonPath("$.[*].hall").value(EventConstants.DB_HALL))
+		.andExpect(jsonPath("$.[*].show").value(EventConstants.DB_SHOW))
+		.andExpect(jsonPath("$.[*].eventDate").value(EventConstants.DB_EVENTDATE))
+		.andExpect(jsonPath("$.[*].price").value(EventConstants.DB_PRICE));
+	}
+	
+	@Test
+	public void testGetInstitutionEvents() throws Exception {
+		this.mockMvc.perform(get(URL_PREFIX + "/getEvents/" + EventConstants.DB_ID)).andExpect(status().isOk())
+		.andExpect(content().contentType(contentType)).andExpect(jsonPath("$", hasSize(3)))
+		.andExpect(jsonPath("$.[*].groups").value(EventConstants.DB_EVENT));
 	}
 
 }
--- Reservation_App/src/test/java/test/constants/EventConstants.java
+++ Reservation_App/src/test/java/test/constants/EventConstants.java
@@ -1,8 +1,13 @@
 package test.constants;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import com.reservationapp.model.Event;
 import com.reservationapp.model.Hall;
+import com.reservationapp.model.Reservation;
+import com.reservationapp.model.Seat;
 import com.reservationapp.model.Show;
 
 public class EventConstants {
@@ -12,6 +17,12 @@ public class EventConstants {
 	public static final int NEW_PRICE = 100;
 	public static final Show NEW_SHOW = new Show();
 	
+	public static final List<List<Event>> DB_GROUPS = new ArrayList<List<Event>>();
+	public static final List<Hall> DB_HALLS = new ArrayList<Hall>();
+	public static final List<Reservation> DB_RESERVATIONS = new ArrayList<Reservation>();
+	public static final List<Seat> DB_SEATS = new ArrayList<Seat>();
+	public static final List<Show> DB_SHOWS = new ArrayList<Show>();
+	public static final Event DB_EVENT = new Event();
 	public static final Long DB_ID = 1L;
 	public static final Date DB_EVENTDATE = new Date();
 	public static final Hall DB_HALL = new Hall();
